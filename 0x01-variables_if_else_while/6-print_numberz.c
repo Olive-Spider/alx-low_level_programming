32bc72abd22477367e9b406142a7aaeafdac10ff
root <livyakendikiruja@gmail.com>
2022-12-09 16:27:14 -0800

    Task 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digits in base 10
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar((x % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
