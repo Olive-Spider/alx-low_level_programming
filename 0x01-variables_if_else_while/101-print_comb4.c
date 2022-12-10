038d7af221bb6514dd197e6831524751f517f4c5
root <livyakendikiruja@gmail.com>
2022-12-09 18:21:10 -0800

    Task 12

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+	int hunds = '0';
+
+	for (hunds = '0'; hunds <= '9'; hunds++)
+	{
+		for (tens = '0'; tens <= '9'; tens++)
+		{
+			if (!((ones == tens) || (tens == hunds) || (tens > ones) || (hunds > tens)))
+			{
+			putchar(hunds);
+			putchar(tens);
+			putchar(ones);
+		if (!(ones == '9' && hunds == '7' && tens == '8'))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
