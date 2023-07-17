214361ac634eb2f4921a78907fbe8dcadeb68234
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-07-18 00:30:59 +0300

    base 10 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers
+ * of base 16 in lowercase
+ * 
+ * Return: 0
+*/
+int main(void)
+{
+char hex_digits[] = "0123456789abcdef";
+int i;
+for (i = 0; i < 16; i++)
+{
+putchar(hex_digits[i]);
+}
+
+putchar('\n');
+
+return (0);
+}
