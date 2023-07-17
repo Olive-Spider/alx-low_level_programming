4bf6bc8d735c60cc0d0f5e48992d6b88e40dde3d
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-07-18 00:04:45 +0300

    more numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - all single digit numbers of base 10 
+ * 
+ * Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+
+return (0);
+}
