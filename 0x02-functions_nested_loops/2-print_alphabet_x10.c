dc24bd63ef90bede88504c08d7e61b871831865d
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-07-18 12:40:03 +0300

    print the alphabet 10 times in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ * in lowercase
+ *
+*/
+void print_alphabet_x10(void)
+{
+char lowercase;
+int i;
+for (i = 0; i <= 10; i++)
+{
+for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+{
+putchar(lowercase);
+}
+putchar('\n');
+}
+}
