1b65c22690ae70a74c1499ac90a4e2366d1c664d
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-07-18 15:41:12 +0300

    print the absolute value of an int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: number to be checked
+ * Return: n
+*/
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (-n);
+}
+}
