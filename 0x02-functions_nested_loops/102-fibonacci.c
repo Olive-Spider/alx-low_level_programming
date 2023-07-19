cdf2d8c4c93a2275681bd6308cfdd8089ea8d68f
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-07-19 21:39:03 +0300

    fibonacci number

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first fibonacci numbers
+ * Return: 0
+*/
+int main(void)
+{
+int i;
+int n = 50;
+int fib[n];
+fib[0] = 1;
+fib[1] = 2;
+
+for (i = 2; i < n; i++)
+{
+fib[i] = fib[i - 1] + fib[i - 2];
+}
+
+for (i = 0; i < n; i++)
+{
+printf("%d", fib[i]);
+if (i < n - 1)
+{
+printf(", ");
+}
+}
+printf("\n");
+return (0);
+}
