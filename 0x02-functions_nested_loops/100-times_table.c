c40ac3cb1ad53548a469001ebdd22dbe4bc4ff56
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-07-19 21:00:43 +0300

    prints n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - prints the n times table
+ * @n: number to be printed
+ * Return: 0
+*/
+void print_times_table(int n)
+{
+int i, j;
+if (n < 0 || n > 15)
+{
+return;
+}
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+printf("%4d", i * j);
+}
+printf("\n");
+}
+}
