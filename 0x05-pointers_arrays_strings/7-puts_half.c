887f3a99995b84039483fe5191403e1919777f79
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-07-26 02:45:23 +0300

    prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to be checked
+ * Return: 0
+*/
+void puts_half(char *str)
+{
+int length = strlen(str);
+int start_index;
+
+if (length % 2 == 0)
+{
+start_index = length / 2;
+}
+else
+{
+start_index = (length - 1) / 2;
+}
+while (str[start_index] != '\0')
+{
+putchar(str[start_index]);
+start_index++;
+}
+putchar('\n');
+}
