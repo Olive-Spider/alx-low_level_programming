ed980268e07d16be1bdb4c1185c2bfef2d91d927
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-07-27 18:48:57 +0300

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,23 +8,16 @@
 */
 void print_number(int n)
 {
-int divisor = 1;
-int temp = n;
+unsigned int i;
+i = n;
 if (n < 0)
 {
 _putchar('-');
-n = -n;
+i = -n;
 }
-while (temp >= 10)
+if (i / 10 != 0)
 {
-divisor *= 10;
-temp /= 10;
+print_number(i / 10);
 }
-while (divisor > 0)
-{
-int digit = n / divisor;
-_putchar('0' + digit);
-n %= divisor;
-divisor /= 10;
-}
+_putchar((i % 10) + '0');
 }
