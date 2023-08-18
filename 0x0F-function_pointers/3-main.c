19e88c89a7f1ce4860c45d282e6b3586fbe5b466
Livya <livyakendikiruja@gmail.com>
2023-08-18 03:29:08 +0300

    performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - function
+ * @argc: input
+ * @argv: input
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int num1, num2;
+int result;
+
+if (argc != 4)
+{
+printf("Error\n");
+return (98);
+}
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+
+if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+{
+printf("Error\n");
+return (100);
+}
+
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+while (ops[i].op != NULL)
+{
+if (*(ops[i].op) == *argv[2] && argv[2][1] == '\0')
+{
+result = ops[i].f(num1, num2);
+printf("%d\n", result);
+return (0);
+}
+i++;
+}
+printf("Error\n");
+return (99);
+}
