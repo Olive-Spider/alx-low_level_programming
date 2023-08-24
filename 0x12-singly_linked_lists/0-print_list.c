64bd4b5a262014454a36ac4fbd5caaf3367807a4
Livya <livyakendikiruja@gmail.com>
2023-08-24 23:40:56 +0300

    prints all the elements of a linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: pointer
+ * Return: 0
+ */
+size_t print_list(const list_t *h)
+{
+size_t count = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%lu] %s\n", count, h->str);
+}
+h = h->next;
+count++;
+}
+return (count);
+}
